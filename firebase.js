5c8477504b4df6c980e28abe037d0c735362f187
Dain <kim@uni.minerva.edu>
2024-08-15 00:06:42 +0900

    add firsbase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA6kZkPTvvHXuztAJECs2xTsl47RyNPoYo",
-  authDomain: "pantryapp-d6e29.firebaseapp.com",
-  projectId: "pantryapp-d6e29",
-  storageBucket: "pantryapp-d6e29.appspot.com",
-  messagingSenderId: "558344822906",
-  appId: "1:558344822906:web:9971c3805cadccb4ed9bc3",
-  measurementId: "G-9T6VY3D4NG"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
